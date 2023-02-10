e4a0e7a02c6d221bafe6b76477111e2a1a8a08c8
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-02-10 22:29:27 +0200

    commit Side Bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './views/Dashboard';
 import Navbar from './views/Navbar';
+import NavSide from './views/NavSide';
 
 export default function App() {
   return (
       <div>
+        <NavSide/>
         <div>
         <Navbar/>
-  
+
         </div>
         <Routes>
         <Route path={"/s"} element={<Dashboard x="weeeeeeeee"/>} />
