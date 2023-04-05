6b75f4033c09dda8b096885dd74acf7008e21e83
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-04-05 14:12:57 +0300

    new footer

---
+++ client/src/views/Footer.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+ 
+import { useState } from 'react';
+
+
+ 
+const Footer = (props) => {
+ 
+   
+     return (
+        <footer className='top'>
+            <img src=''/>
+          
+       
+         
+                <h2  >Dive into our community ! {props.x}</h2>
+                <h2  >no limits  </h2>
+
+            
+            
+
+        </footer>
+    )
+}
+
+export default Footer
