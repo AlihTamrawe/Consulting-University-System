8921374f5aa11b88f0338f5d2555ef2730f7634a
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-04-26 23:17:36 +0300

    new

---
+++ client/src/views/ConsultantDash.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+
+ 
+import { useState } from 'react';
+
+
+ 
+const Consaltantdash = (props) => {
+ 
+   
+     return (
+        <div  >
+          
+       
+         
+                <h2  >Consaltantdash ! {props.x}</h2>
+                <h2  >no limits  </h2>
+
+            
+            
+
+        </div>
+    )
+}
+
+export default Consaltantdash
